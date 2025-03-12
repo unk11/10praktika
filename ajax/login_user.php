8101d4e71814f778a8e86a185c82c59601ef0a7f
unk <ununk@DESKTOP-NH0291J>
2025-03-13 01:14:56 +0500

    шаг 9 На PHP странице сервера, обрабатывающей код, использовав библиотеку, указать следующий код, изменил секретный ключ Прохоров

--- ajax/login_user.php
+++ ajax/login_user.php
@@ -1,20 +1,49 @@
 <?php
-	session_start();
-	include("../settings/connect_datebase.php");
-	
-	$login = $_POST['login'];
-	$password = $_POST['password'];
-	
-	// ищем пользователя
-	$query_user = $mysqli->query("SELECT * FROM `users` WHERE `login`='".$login."' AND `password`= '".$password."';");
-	
-	$id = -1;
-	while($user_read = $query_user->fetch_row()) {
-		$id = $user_read[0];
-	}
-	
-	if($id != -1) {
-		$_SESSION['user'] = $id;
-	}
-	echo md5(md5($id));
+session_start();
+include("../settings/connect_datebase.php");
+include("../recaptcha/autoload.php");
+
+header('Content-Type: application/json');
+
+$secret = "6LfGnfIqAAAAALvOB-QhDYsNBAUcdQGN0gpibNcV";
+
+if (!isset($_POST['g-recaptcha-response'])) {
+    error_log("reCAPTCHA response is missing!");
+    echo json_encode(["status" => "error", "message" => "Ошибка: reCAPTCHA не была отправлена."]);
+    exit;
+}
+
+$recaptcha = new \ReCaptcha\ReCaptcha($secret);
+$ip = $_SERVER['HTTP_X_FORWARDED_FOR'] ?? $_SERVER['REMOTE_ADDR'];
+$resp = $recaptcha->verify($_POST['g-recaptcha-response'], $ip);
+
+if (!$resp->isSuccess()) {
+    $errors = $resp->getErrorCodes();
+    error_log("reCAPTCHA errors: " . print_r($errors, true));
+    echo json_encode(["status" => "error", "message" => "Ошибка: reCAPTCHA не пройдена."]);
+    exit;
+}
+
+$login = $_POST['login'] ?? '';
+$password = $_POST['password'] ?? '';
+
+$query_user = $mysqli->query("SELECT * FROM `users` WHERE `login`='" . $mysqli->real_escape_string($login) . "' AND `password`='" . $mysqli->real_escape_string($password) . "';");
+
+$id = -1;
+$role = -1;
+
+while ($user_read = $query_user->fetch_row()) {
+    $id = $user_read[0];
+    $role = $user_read[3];
+}
+
+if ($id == -1) {
+    echo json_encode(["status" => "error", "message" => "Ошибка: Неверный логин или пароль."]);
+    exit;
+}
+
+$_SESSION['user'] = $id;
+$redirect_url = ($role == 1) ? "admin.php" : "user.php";
+
+echo json_encode(["status" => "success", "redirect" => $redirect_url]);
 ?>
--- login.php
+++ login.php
@@ -1,129 +1,133 @@
 <?php
-	session_start();
-	include("./settings/connect_datebase.php");
-	
-	if (isset($_SESSION['user'])) {
-		if($_SESSION['user'] != -1) {
-			
-			$user_query = $mysqli->query("SELECT * FROM `users` WHERE `id` = ".$_SESSION['user']);
-			while($user_read = $user_query->fetch_row()) {
-				if($user_read[3] == 0) header("Location: user.php");
-				else if($user_read[3] == 1) header("Location: admin.php");
-			}
-		}
- 	}
-?>
-<html>
-	<head> 
-		<meta charset="utf-8">
-		<title> Авторизация </title>
-		<script src="https://www.google.com/recaptcha/api.js" async defer></script>
-		<script src="https://code.jquery.com/jquery-1.8.3.js"></script>
-		<link rel="stylesheet" href="style.css">
-	</head>
-	<body>
-		<div class="top-menu">
-			<a href=#><img src = "img/logo1.png"/></a>
-			<div class="name">
-				<a href="index.php">
-					<div class="subname">БЗОПАСНОСТЬ  ВЕБ-ПРИЛОЖЕНИЙ</div>
-					Пермский авиационный техникум им. А. Д. Швецова
-				</a>
-			</div>
-		</div>
-		<div class="space"> </div>
-		<div class="main">
-			<div class="content">
-				<div class = "login">
-					<div class="name">Авторизация</div>
-				
-					<div class = "sub-name">Логин:</div>
-					<input name="_login" type="text" placeholder="" onkeypress="return PressToEnter(event)"/>
-					<div class = "sub-name">Пароль:</div>
-					<input name="_password" type="password" placeholder="" onkeypress="return PressToEnter(event)"/>
-					
-					<center><div class="g-recaptcha" data-sitekey="6LcNnfIqAAAAAHbVOrEcurFDvD7I_nlHoJPxCJ7Y"></div></center>
+session_start();
+include("./settings/connect_datebase.php");
 
-					<a href="regin.php">Регистрация</a>
-					<br><a href="recovery.php">Забыли пароль?</a>
-					<input type="button" class="button" value="Войти" onclick="LogIn()"/>
-					<img src = "img/loading.gif" class="loading"/>
-				</div>
-				
-				<div class="footer">
-					© КГАПОУ "Авиатехникум", 2020
-					<a href=#>Конфиденциальность</a>
-					<a href=#>Условия</a>
-				</div>
-			</div>
-		</div>
-		
-		<script>
-			function LogIn() {
-				var loading = document.getElementsByClassName("loading")[0];
-				var button = document.getElementsByClassName("button")[0];
-				
-				var _login = document.getElementsByName("_login")[0].value;
-				var _password = document.getElementsByName("_password")[0].value;
-				var captcha = grecaptcha.getResponse();
-				if (!captcha.length) {
-					alert("Вы не прошли проверку reCAPTCHA!");
-					return;
-				}
-				loading.style.display = "block";
-				button.className = "button_diactive";
-				
-				var data = new FormData();
-				data.append("login", _login);
-				data.append("password", _password);
-				
-				// AJAX запрос
-				$.ajax({
-					url         : 'ajax/login_user.php',
-					type        : 'POST', // важно!
-					data        : data,
-					cache       : false,
-					dataType    : 'html',
-					// отключаем обработку передаваемых данных, пусть передаются как есть
-					processData : false,
-					// отключаем установку заголовка типа запроса. Так jQuery скажет серверу что это строковой запрос
-					contentType : false, 
-					// функция успешного ответа сервера
-					success: function (_data) {
-						console.log("Авторизация прошла успешно, id: " +_data);
-						if(_data == "") {
-							loading.style.display = "none";
-							button.className = "button";
-							alert("Логин или пароль не верный.");
-						} else {
-							localStorage.setItem("token", _data);
-							location.reload();
-							loading.style.display = "none";
-							button.className = "button";
-						}
-					},
-					// функция ошибки
-					error: function( ){
-						console.log('Системная ошибка!');
-						loading.style.display = "none";
-						button.className = "button";
-					}
-				});
-			}
-			
-			function PressToEnter(e) {
-				if (e.keyCode == 13) {
-					var _login = document.getElementsByName("_login")[0].value;
-					var _password = document.getElementsByName("_password")[0].value;
-					
-					if(_password != "") {
-						if(_login != "") {
-							LogIn();
-						}
-					}
-				}
-			}
-			
-		</script>
-	</body>
+// Если пользователь уже авторизован, перенаправляем его
+if (isset($_SESSION['user']) && $_SESSION['user'] != -1) {
+    $stmt = $mysqli->prepare("SELECT role FROM users WHERE id = ?");
+    $stmt->bind_param("i", $_SESSION['user']);
+    $stmt->execute();
+    $stmt->bind_result($role);
+    $stmt->fetch();
+    $stmt->close();
+
+    if ($role === 0) {
+        header("Location: user.php");
+        exit();
+    } elseif ($role === 1) {
+        header("Location: admin.php");
+        exit();
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="ru">
+<head> 
+    <meta charset="utf-8">
+    <title>Авторизация</title>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="top-menu">
+        <a href="#"><img src="img/logo1.png"/></a>
+        <div class="name">
+            <a href="index.php">
+                <div class="subname">БЕЗОПАСНОСТЬ ВЕБ-ПРИЛОЖЕНИЙ</div>
+                Пермский авиационный техникум им. А. Д. Швецова
+            </a>
+        </div>
+    </div>
+    <div class="space"></div>
+    <div class="main">
+        <div class="content">
+            <div class="login">
+                <div class="name">Авторизация</div>
+
+                <div class="sub-name">Логин:</div>
+                <input name="_login" type="text" placeholder="Введите логин" onkeypress="return PressToEnter(event)"/>
+                
+                <div class="sub-name">Пароль:</div>
+                <input name="_password" type="password" placeholder="Введите пароль" onkeypress="return PressToEnter(event)"/>
+
+                <center>
+                    <div class="g-recaptcha" data-sitekey="6LfGnfIqAAAAAJxZWJKJRq-QI_cP2zH-wUHxp43h"></div>
+                </center>
+
+                <a href="regin.php">Регистрация</a>
+                <br><a href="recovery.php">Забыли пароль?</a>
+
+                <input type="button" class="button" value="Войти" onclick="LogIn()"/>
+                <img src="img/loading.gif" class="loading" style="display: none;"/>
+            </div>
+
+            <div class="footer">
+                © КГАПОУ "Авиатехникум", 2020
+                <a href="#">Конфиденциальность</a>
+                <a href="#">Условия</a>
+            </div>
+        </div>
+    </div>
+
+    <script>
+        function LogIn() {
+            var loading = document.querySelector(".loading");
+            var button = document.querySelector(".button");
+
+            var _login = document.querySelector("[name='_login']").value.trim();
+            var _password = document.querySelector("[name='_password']").value.trim();
+            var captcha = grecaptcha.getResponse();
+
+            if (!_login || !_password) {
+                alert("Пожалуйста, заполните все поля!");
+                return;
+            }
+            if (!captcha.length) {
+                alert("Вы не прошли проверку reCAPTCHA!");
+                return;
+            }
+
+            loading.style.display = "block";
+            button.disabled = true;
+
+            var data = new FormData();
+            data.append("login", _login);
+            data.append("password", _password);
+            data.append("g-recaptcha-response", captcha);
+
+            $.ajax({
+                url: 'ajax/login_user.php',
+                type: 'POST',
+                data: data,
+                cache: false,
+                dataType: 'json',
+                processData: false,
+                contentType: false,
+                success: function (response) {
+                    if (response.status === "success") {
+                        window.location.href = response.redirect;
+                    } else {
+                        alert(response.message);
+                        grecaptcha.reset();
+                        loading.style.display = "none";
+                        button.disabled = false;
+                    }
+                },
+                error: function () {
+                    alert("Ошибка связи с сервером!");
+                    loading.style.display = "none";
+                    button.disabled = false;
+                }
+            });
+        }
+
+        function PressToEnter(e) {
+            if (e.keyCode === 13) {
+                LogIn();
+            }
+        }
+    </script>
+</body>
 </html>
